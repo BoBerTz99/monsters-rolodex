0eea4adf3266d05dc1e2646d31fef0004561724f
Roberto UserGit <robrt.oztyle.92@gmail.com>
2021-08-27 13:08:41 -0500

    Primer commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import { placeholder } from '@babel/types';
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <input 
+        className='search'
+        type='search'
+        placeholder={placeholder}
+        onChange={handleChange}
+    />
+);
